b62592ff8824a486766b445affce9916065829cb
Yousef Attia <yousef1907@hotmail.com>
2024-09-07 02:32:23 +0200

    Fixed landmarks for accessibility

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -17,7 +17,10 @@ export default function Newsletter() {
 		alert("Thank you for subscribing!");
 	}
 	return (
-		<section className="flex flex-col items-center min-h-96 pb-10 lg:pb-0 md:h-80 bg-primarySoftBlue justify-center gap-10 px-10 lg:w-full">
+		<section
+			className="flex flex-col items-center min-h-96 pb-10 lg:pb-0 md:h-80 bg-primarySoftBlue justify-center gap-10 px-10 lg:w-full"
+			role="region"
+		>
 			<h2 className="text-white text-sm">35,000+ already joined</h2>
 			<p className="text-white text-3xl font-medium">
 				Stay up-to-date with what we're doing
